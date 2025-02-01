a1cac5821445453f83edb6ab4fe0379798fb82bc
Quentin Biache <nitrogenium@outlook.fr>
2025-02-01 22:20:17 +0100

    bugfix on the start/stop button added multiple oscillators

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let osc1 = null, osc2 = null, gainNode = null;
+let oscillators = [];
+let gains = [];
+let masterGain = audioCtx.createGain();
 let isPlaying = false;
 
 const N_MAX_HARMONICS = 20;
 
-const freq1Slider = document.getElementById("freq1");
-const freq2Slider = document.getElementById("freq2");
-const freq1ValueDisplay = document.getElementById("freq1Value");
-const freq2ValueDisplay = document.getElementById("freq2Value");
+// const freq1Slider = document.getElementById("freq1");
+// const freq2Slider = document.getElementById("freq2");
+// const freq1ValueDisplay = document.getElementById("freq1Value");
+// const freq2ValueDisplay = document.getElementById("freq2Value");
 const startStopButton = document.getElementById("startStop");
 
 function startOscillators() 
 {
   if (!isPlaying) 
   {
-    osc1 = audioCtx.createOscillator();
-    osc2 = audioCtx.createOscillator();
-    gainNode = audioCtx.createGain(); // To control volume if needed
+    for (let i = 0; i < 10; i++)
+    {
+      let osc = audioCtx.createOscillator();
+      let gain = audioCtx.createGain();
 
-    osc1.type = "sine";
-    osc2.type = "sine";
-    osc1.frequency.value = freq1Slider.value;
-    osc2.frequency.value = freq2Slider.value;
+      osc.type = "sine";
+      osc.frequency.value = f0Slider.value*(i+1);
+      gain.gain.value = 0.01;
 
-    // Mix the two oscillators by connecting them to the same gain node
-    osc1.connect(gainNode);
-    osc2.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
+      osc.connect(gain);
+      gain.connect(masterGain);
 
-    osc1.start();
-    osc2.start();
+      osc.start();
+
+      oscillators.push(osc);
+      gains.push(gain);
+    }
+    
+    masterGain.connect(audioCtx.destination);
+    masterGain.gain.value = volSlider.value;
+
     isPlaying = true;
     startStopButton.textContent = "Stop";
   }
@@ -39,15 +46,47 @@ function stopOscillators()
 {
   if (isPlaying) 
   {
-    osc1.stop();
-    osc2.stop();
-    osc1.disconnect();
-    osc2.disconnect();
-    gainNode.disconnect();
+    oscillators.forEach((osc, index) => 
+    {
+      osc.stop();
+      osc.disconnect();
+      gains[index].disconnect();
+    }
+    );
+
+    // Clear arrays
+    oscillators = [];
+    gains = [];
+
     isPlaying = false;
     startStopButton.textContent = "Start";
+    
+    
+    // osc1.stop();
+    // osc2.stop();
+    // osc1.disconnect();
+    // osc2.disconnect();
+    // gainNode.disconnect();
+    // isPlaying = false;
+    // startStopButton.textContent = "Start";
   }
 }
+
+
+
+// ============================================================================
+// EVENT LISTENERS
+// ============================================================================
+volSlider.addEventListener("input", 
+  function() 
+  {
+    volSliderDisplay.textContent = volSlider.value;
+    if (isPlaying) 
+    {
+      masterGain.gain.setValueAtTime(volSlider.value, audioCtx.currentTime);
+    }
+  }
+);
 
 f0Slider.addEventListener("input", 
   function() 
@@ -55,7 +94,10 @@ f0Slider.addEventListener("input",
     f0ValDisplay.textContent = f0Slider.value + ' Hz';
     if (isPlaying) 
     {
-      osc1.frequency.setValueAtTime(f0Slider.value, audioCtx.currentTime);
+      for (let i = 0; i < 10; i++)
+      {
+        oscillators[i].frequency.setValueAtTime(f0Slider.value*(i+1), audioCtx.currentTime);
+      }
     }
   }
 );
@@ -75,13 +117,15 @@ decaySlider.addEventListener("input",
   function() 
   {
     decayValDisplay.textContent = '(1/n)^' + decaySlider.value;
-    // if (isPlaying) 
-    // {
-    //   osc2.frequency.setValueAtTime(freq2Slider.value, audioCtx.currentTime);
-    // }
+    if (isPlaying) 
+    {
+      for (let i = 0; i < 10; i++)
+      {
+        gains[i].gain.setValueAtTime(0.01*Math.pow(1 / (i+1), decaySlider.value), audioCtx.currentTime);
+      }
+    }
   }
 );
-
 
 startStopButton.addEventListener("click", 
   function() 
